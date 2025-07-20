7285b2f99b37946992f1278137d54c0e17364aee
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-07-20 22:58:46 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./state/store.js";
-
+import { Analytics } from "@vercel/analytics/react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
