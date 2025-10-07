d362459f5a3ccc0490e70848987f49337b4de506
Kaustubh Madhuri Ramesh Aher <119882850+aherkaustubh16t@users.noreply.github.com>
2025-10-07 19:40:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DSASection from "./components/DSASection/DSASection";
 import ExperienceMain from "./components/experienceSection/ExperienceMain";       
 import FooterMain from "./components/footer/FooterMain";  
 import HeroGradient from "./components/heroSection/HeroGradient";        
-import HeroMain from "./components/heroSection/HeroMain";       
+import HeroMain from "./components/heroSection/HeroMain";        
 import NavbarMain from "./components/navbar/NavbarMain";    
 import ProjectsMain from "./components/projectsSection/ProjectsMain";     
 import SkillsMain from "./components/skillsSection/SkillsMain";  
