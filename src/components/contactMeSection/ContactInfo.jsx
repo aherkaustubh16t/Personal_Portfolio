0fb5351811bb69919391ad6e0b423fb855828b2a
Kaustubh Madhuri Ramesh Aher <119882850+aherkaustubh16t@users.noreply.github.com>
2025-10-16 19:57:22 +0530

    updated

--- src/components/contactMeSection/ContactInfo.jsx
+++ src/components/contactMeSection/ContactInfo.jsx
@@ -1,4 +1,4 @@
-import { HiOutlineMail } from "react-icons/hi";
+stuimport { HiOutlineMail } from "react-icons/hi";
 import { FiPhone } from "react-icons/fi";
 import { IoLocationOutline } from "react-icons/io5";
 import SingleInfo from "./SingleInfo";
@@ -7,7 +7,7 @@ const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-4">
       {/* Email - plain text (not clickable) */}
-      <SingleInfo text="abdelrahman.qassem@gmail.com" Image={HiOutlineMail} />
+      <SingleInfo text="aherkaustubhramesh16@gmail.com" Image={HiOutlineMail} />
 
       {/* Phone - tel link */}
       <a href="tel:+917249734437" className="hover:text-orange">
