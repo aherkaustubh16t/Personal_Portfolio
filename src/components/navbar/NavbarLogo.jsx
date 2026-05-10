50dcaee808eb194f1da4f7c5e934f0e6d460549e
Kaustubh Madhuri Ramesh Aher <119882850+aherkaustubh16t@users.noreply.github.com>
2026-05-10 20:39:36 +0530

    Wrap logo in anchor tag for navigation

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,11 +1,13 @@
 const NavbarLogo = () => {
   return (
     <div className="w-40 h-20 flex items-center justify-center rounded-full overflow-hidden">
-      <img
-        src="/images/logo.png" // Replace with your logo path
-        alt="Logo"
-        className="w-full h-full object-cover"
-      />
+      <a href="/">
+  <img
+    src="/images/logo.png"
+    alt="Logo"
+    className="w-full h-full object-cover"
+  />
+</a>
     </div>
   );
 };
